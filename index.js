aa5993e2c6c265fc643530437d081b9cdbfd9b09
j3tl33 <47133368+j3tl33@users.noreply.github.com>
2019-09-26 23:36:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ message.member.addRole(role);
 })
 
 
-bot.login('NjI2NzUzODEwMTk4NTYwNzY5.XYywpA.0Xe89NuQ7XHgCj_Arm-6RUWQTPM');
+bot.login('NjI2NzUzODEwMTk4NTYwNzY5.XYy-rQ.RhGWlBYIK46B3VQUF-Uncg7C6RM');
